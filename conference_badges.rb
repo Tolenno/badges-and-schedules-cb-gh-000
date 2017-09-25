4cb871908b5c52556d0324a6aa911f27b138f096
Matt Hough <matt@hough.tv>
2017-09-25 11:07:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect.with_index do |name, index|
+  array.collect.with_index do |name|
     name = badge_maker(name)
   end
 end
